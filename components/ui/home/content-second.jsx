983dd49e33a512f47bfd0d39b3cf6222a2f507e3
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-11-30 22:32:38 +0700

    feat: update

--- components/ui/home/content-second.jsx
+++ components/ui/home/content-second.jsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Spectral from "@/lib/fonts/spectral";
+import BlurFade from "@/components/ui/blur-fade";
 import { Button } from "@/components/ui/button";
 import { Parallax } from "react-parallax";
 import { IconPhoto } from "@tabler/icons-react";
@@ -10,15 +11,19 @@ export default function ContentSecond() {
       <div
         className={`${Spectral.className} max-w-xl font-light text-3xl text-center w-full leading-none tracking-normal mt-80`}
       >
-        — A Story of Elegance and Identity —
+        <BlurFade delay={0.25 + 1 * 0.05} inView>
+          — A Story of Elegance and Identity —
+        </BlurFade>
       </div>
       <div className="max-w-xl font-extralight text-md text-gray-300 text-justify w-full mt-10">
-        Rooted in the philosophy of{" "}
-        <span className="font-semibold text-white">
-          &quot;The Luxurious Sense of Belonging&quot;
-        </span>
-        , each kebaya is designed to radiate confidence and charm, allowing
-        every woman to embrace her beauty with pride.
+        <BlurFade delay={0.25 + 2 * 0.05} inView>
+          Rooted in the philosophy of{" "}
+          <span className="font-semibold text-white">
+            &quot;The Luxurious Sense of Belonging&quot;
+          </span>
+          , each kebaya is designed to radiate confidence and charm, allowing
+          every woman to embrace her beauty with pride.
+        </BlurFade>
       </div>
       <div className="w-full mt-16">
         <Parallax
@@ -30,23 +35,29 @@ export default function ContentSecond() {
         </Parallax>
       </div>
       <div className="max-w-xl font-extralight text-md text-gray-300 text-justify w-full mt-16">
-        At <span className="font-semibold text-white">Yunius Mujianto</span>, we
-        believe in creating more than just fashion.
+        <BlurFade delay={0.25 + 3 * 0.05} inView>
+          At <span className="font-semibold text-white">Yunius Mujianto</span>,
+          we believe in creating more than just fashion.
+        </BlurFade>
       </div>
       <div className="max-w-xl font-extralight text-md text-gray-300 text-justify w-full mt-6">
-        Experience the timeless artistry and modern refinement that have made{" "}
-        <span className="font-semibold text-white">Yunius Kebaya Bride</span> a
-        symbol of luxury and individuality in the world of fashion.
+        <BlurFade delay={0.25 + 4 * 0.05} inView>
+          Experience the timeless artistry and modern refinement that have made{" "}
+          <span className="font-semibold text-white">Yunius Kebaya Bride</span>{" "}
+          a symbol of luxury and individuality in the world of fashion.
+        </BlurFade>
       </div>
       <div className="max-w-xl text-right w-full mt-16 pointer-events-auto">
-        <Link href="/gallery">
-          <Button
-            size="lg"
-            className="hover:shadow-2xl hover:scale-105 transition-transform duration:500"
-          >
-            SEE OUR GALLERY <IconPhoto className="w-5 h-5" />
-          </Button>
-        </Link>
+        <BlurFade delay={0.25 + 5 * 0.05} inView>
+          <Link href="/gallery">
+            <Button
+              size="lg"
+              className="hover:shadow-2xl hover:scale-105 transition-transform duration:500"
+            >
+              SEE OUR GALLERY <IconPhoto className="w-5 h-5" />
+            </Button>
+          </Link>
+        </BlurFade>
       </div>
     </>
   );
