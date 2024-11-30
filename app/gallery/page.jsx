7ee7310d1a0925f822dbb8796fdcee06f584b08c
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-11-30 14:56:09 +0700

    feat: update

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -6,6 +6,7 @@ export default function Page() {
     <>
       <Header />
       <Tiles />
+      <Footer />
     </>
   );
 }
@@ -15,6 +16,22 @@ function Header() {
     <div className="fixed z-40 top-0 start-0 w-full mt-10 px-10 text-center">
       <div className="inline-block mx-auto py-2 px-6">
         <Navigation />
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="fixed bottom-0 start-0 w-full mb-10 px-10 grid grid-cols-3">
+      <div className="flex justify-start font-extralight text-sm text-white pt-6">
+        by YUNIUS MUJIANTO
+      </div>
+      <div className="flex justify-center font-extralight text-sm text-white/80 pt-6">
+        Copyright © Yunius Mujianto 2024. All rights reserved.
+      </div>
+      <div className="flex justify-end font-extralight text-sm text-white pt-6">
+        01/12/2024
       </div>
     </div>
   );
