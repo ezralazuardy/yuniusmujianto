591c5cf2b7e00000b21c86304ba811421d28cb3a
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-12-03 14:30:46 +0700

    feat: update

--- app/layout.jsx
+++ app/layout.jsx
@@ -22,6 +22,21 @@ export default function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={metadata.description} />
         <meta name="robots" content="index, follow" />
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <meta name="apple-mobile-web-app-title" content="Yunius" />
+        <link rel="manifest" href="/site.webmanifest" />
 
         {/* Open Graph Meta */}
         <meta property="og:title" content={metadata.title} />
