22d8d4fd195ecfdc08b85da3505737c82f32160a
Ezra Lazuardy <ezralazuardy@icloud.com>
2024-12-03 14:58:39 +0700

    feat: update

--- next.config.mjs
+++ next.config.mjs
@@ -4,22 +4,22 @@ const nextConfig = {
   devIndicators: {
     appIsrStatus: false,
   },
-  images: {
-    minimumCacheTTL: 2592000, // cache for 1 month (2592000 seconds)
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=2592000, must-revalidate", // cache for 1 month (2592000 seconds)
-          },
-        ],
-      },
-    ];
-  },
+  // images: {
+  //   minimumCacheTTL: 2592000, // cache for 1 month (2592000 seconds)
+  // },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: "/(.*)",
+  //       headers: [
+  //         {
+  //           key: "Cache-Control",
+  //           value: "public, max-age=2592000, must-revalidate", // cache for 1 month (2592000 seconds)
+  //         },
+  //       ],
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
